55b79f8efb13ff68656c9ebef665bc63fe4c3999
Zenith Code Services <ZenithCodeServices@tims-mbp.home>
2022-03-26 22:08:34 +0000

    Small UI changes

--- components/NavRightButton.js
+++ components/NavRightButton.js
@@ -1,17 +1,20 @@
 import{ classNames } from '../utils/helpers'
+import Link from 'next/link'
 
 function NavRightButton({ text, layout }) {
   return (
-    <p
-      href="/login?signup=1 "
-      style={{ textDecoration: 'none', cursor: 'pointer'}}
-      className={classNames(
-        layout === 'desktop' ? 'px-4 py-2' : 'px-6 py-3',
-        'bg-palette-primary rounded-md text-black font-medium hover:bg-palette-dark'
-      )}      
-    >
-      {text}
-    </p>
+    <Link href="/login?signup=1">
+      <p
+        href="/login?signup=1"
+        style={{ textDecoration: 'none', cursor: 'pointer'}}
+        className={classNames(
+          layout === 'desktop' ? 'px-4 py-2' : 'px-6 py-3',
+          'bg-palette-primary rounded-md text-black font-medium hover:bg-palette-dark'
+        )}
+      >
+        {text}
+      </p>
+    </Link>
   )
 }
 
